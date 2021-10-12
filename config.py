090946c27cbb0d96cedde94d1ea6879cf9f4824a
binyu <binyu@fiture.com>
2021-10-12 11:31:56 +0800

    adding config file

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 # 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
 
-
-database_path = ''
 # todo 上生产前需要修改
 source_root = r'D:\Jupyter\tt_spider'
+
+local_host = 'localhost'
+local_port = 3306
+local_user = 'root'
+local_passwd = 'password'
+
+db_host = 'hostname'
+db_port = 3306
+db_user = 'username'
+db_passwd = 'password'
